b5955cde6f6c988b6a8e6111a0424563b5ccc5a3
Eric Bear <eric.lj.bear@gmail.com>
2023-04-27 16:45:13 -0600

    complete first lab

---
+++ Testing/drawing_samples.py
@@ -0,0 +1,63 @@
+'''
+Sample program to show how to draw using Python and the 
+Arcade library.
+'''
+# import the arcade library
+import arcade
+
+# Open up a window that is 600 x 600 and set window title.
+arcade.open_window(600, 600, "Drawing Example")
+
+# Set the background color
+arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+
+# Get ready to draw
+arcade.start_render()
+
+# Drawing code goes here.
+# Draw a rectangle. Left of 0, Right of 599, Top of 300
+# bottom of 0
+# Grass
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+# Tree circle
+arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
+# Tree elllipse
+arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
+# Tree arc
+arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
+# Tree Triangle
+arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.DARK_GREEN)
+# Draw a tree using a polygon with a list of points
+arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.SIENNA)
+arcade.draw_polygon_filled(((500, 400),
+                            (480, 360),
+                            (470, 320),
+                            (530, 320),
+                            (520, 360)
+                            ),
+                           arcade.csscolor.DARK_GREEN)
+# Draw sun
+# Draw a sun
+arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
+
+# Rays to the left, right, up, and down
+arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 500, 650, arcade.color.YELLOW, 3)
+
+# Diagonal rays
+arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
+
+# Finish Drawing
+arcade.finish_render()
+
+# Keep the window open until we close it
+arcade.run()
